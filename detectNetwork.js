92a2f2ca193b07a1db4a3eafb44f54d1852ae0f8
Justin Yoo <justindulark@gmail.com>
2017-10-30 18:58:08 -0700

    Added functionality to detect China UnionPay

--- detectNetwork.js
+++ detectNetwork.js
@@ -22,15 +22,23 @@ var detectNetwork = function(cardNumber) {
   else if (cardNumber.slice(0,1) === '4' && (cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19)) {
   	return "Visa";
   }
-  else if ((cardNumber.slice(0,2) === '51' || cardNumber.slice(0,2) === '52' || cardNumber.slice(0,2) === '53' || cardNumber.slice(0,2) === '54' || cardNumber.slice(0,2) === '55') && (cardNumber.length === 16)) {
+  else if ((cardNumber.slice(0,2) === '51' || cardNumber.slice(0,2) === '52' || cardNumber.slice(0,2) === '53' 
+  	|| cardNumber.slice(0,2) === '54' || cardNumber.slice(0,2) === '55') && (cardNumber.length === 16)) {
   	return "MasterCard";
   }
-  else if (((cardNumber.slice(0,4) === '6011' || cardNumber.slice(0,2) === '65') || (cardNumber.slice(0,3) >= '644' && cardNumber.slice(0,3) <= '649')) && (cardNumber.length === 16 || cardNumber.length === 19)) {
+  else if (((cardNumber.slice(0,4) === '6011' || cardNumber.slice(0,2) === '65') || 
+  	(parseInt(cardNumber.slice(0,3)) >= 644 && parseInt(cardNumber.slice(0,3)) <= 649)) && (cardNumber.length === 16 || cardNumber.length === 19)) {
   	return 'Discover';
   }
-  else if ((cardNumber.slice(0,4) === '5018' || cardNumber.slice(0,4) === '5020' || cardNumber.slice(0,4) === '5038' || cardNumber.slice(0,4) === '6304') && (cardNumber.length >= 12 && cardNumber.length <= 19)) {
+  else if ((cardNumber.slice(0,4) === '5018' || cardNumber.slice(0,4) === '5020' || cardNumber.slice(0,4) === '5038' || 
+  	cardNumber.slice(0,4) === '6304') && (cardNumber.length >= 12 && cardNumber.length <= 19)) {
   	return 'Maestro';
   }
+  else if ( (parseInt(cardNumber.slice(0,6)) >= 622126 && parseInt(cardNumber.slice(0,6)) <= 622925) || 
+  	(parseInt(cardNumber.slice(0,3) >= 624 && parseInt(cardNumber.slice(0,3)) <= 626)) && (cardNumber.length >= 16 && cardNumber.length <= 19)) {
+  	return 'China UnionPay'
+  }
+
   return 'None'
 };
 
